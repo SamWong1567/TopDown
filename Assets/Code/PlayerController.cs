469a7a057343174469dad2f0eb92dc82fa8027b1
erjillsison <erjillsison@gmail.com>
2016-10-13 16:05:51 +0800

    Controller changes

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -8,8 +8,9 @@ public class PlayerController : MonoBehaviour {
     private float runSpeed = 5;
 
     public VirtualJoystick joystick;
-    public VirtualJoystick1 joystickLook;
+    public VirtualJoystick joystickLook;
     
+   
 
     private Quaternion targetRotation;
 
@@ -21,8 +22,10 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
 
         controller = GetComponent<CharacterController>();
-        
-	}
+        joystickLook.transform.position = new Vector2(joystickLook.canvasSize.x*3 / 4, joystickLook.canvasSize.y / 2);
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,16 +33,16 @@ public class PlayerController : MonoBehaviour {
         Vector3 input = new Vector3 (joystick.Horizontal(), 0, joystick.Vertical());
         Vector3 lookInput = new Vector3(joystickLook.Horizontal(), 0, joystickLook.Vertical());
 
-
+        //Rotate player
         if (lookInput != Vector3.zero) {
             targetRotation = Quaternion.LookRotation(lookInput);
             transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
         }
 
+
+        //Move player
         Vector3 motion = input.normalized;
         motion += Vector3.up * -8;
-
-
         controller.Move(motion*walkSpeed * Time.deltaTime);
 
         
--- Assets/Code/VirtualJoystick.cs
+++ Assets/Code/VirtualJoystick.cs
@@ -7,111 +7,132 @@ public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, I
 
     private Image bgImg;
     private Image joystickImg;
-    private Vector3 originPoint;
-    private Vector3 dragPoint;
-    private Vector3 move;
+    private Image joystickKnob;
+    private Vector2 move;
     private RectTransform canvas;
     private RectTransform joystickRect;
 
-    private Vector2 canvasSize;
+    public Vector2 canvasSize;
     private Vector2 joystickOrigin;
 
     // Use this for initialization
     void Start () {
 
+        //Get components
         bgImg = GetComponent<Image>();
         joystickImg = transform.GetChild(0).GetComponent<Image>();
         canvas = transform.parent.gameObject.GetComponent<RectTransform>();
         joystickRect = GetComponent<RectTransform>();
+        joystickKnob = joystickImg.transform.GetChild(0).GetComponent<Image>();
 
-
+        //joystick disabled on start
         joystickImg.enabled = false;
+        joystickKnob.enabled = false;
 
+        //change color alpha of joystick boundaries to transparent
         Color color = bgImg.color;
         color.a = 0;
         bgImg.color = color;
 
+        //get dimension of canvas
         canvasSize = new Vector2(canvas.rect.width, canvas.rect.height);
 
-        Debug.Log(canvasSize);
-
-        transform.position = new Vector2(canvasSize.x/4,canvasSize.y/2);
+        
+        //set size of joystick to half of screen size        
         joystickRect.sizeDelta = new Vector2(canvasSize.x/2,canvasSize.y);
+        transform.position = new Vector2(canvasSize.x / 4, canvasSize.y / 2);
 
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
+    //Event: when cursor is dragged after clicking
     public virtual void OnDrag(PointerEventData ped) {
-        Vector2 pos;
 
+        Vector2 pos;
+        //Convert screen point coord to local point in joystick rect
         if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos)) {
 
 
+            ////divide the coords to the size of joystick background and multiply by 2 to get coords max as 1
+            //pos.x = (pos.x * 2 / bgImg.rectTransform.sizeDelta.x);
+            //pos.y = (pos.y * 2 / bgImg.rectTransform.sizeDelta.y);
 
 
-            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
-            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+            ////the current coord
+            //dragPoint = new Vector3(pos.x, 0, pos.y);
 
+            ////to get the difference and find the net movement
+            //move = dragPoint - originPoint;
 
+            //to prevent moving diagonally to be faster
+            //move = (move.magnitude > 1.0f) ? move.normalized : move;
 
-
-            dragPoint = new Vector3(pos.x * 2, 0, pos.y * 2);
-            move = dragPoint - originPoint;
            
-            move = (move.magnitude > 1.0f) ? move.normalized : move;
-            Debug.Log(move);
 
 
+            ////visually move joystick
+            //Vector2 joystickMove = new Vector2(move.x, move.z) * 30;
+            //joystickImg.rectTransform.anchoredPosition = joystickOrigin + joystickMove;
 
-            Vector2 joystickMove = new Vector2(move.x, move.z) * 100;
-            joystickImg.rectTransform.anchoredPosition = joystickOrigin + joystickMove;
-                ;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickImg.rectTransform, ped.position, ped.pressEventCamera, out pos)) {
+
+                //Debug.Log(pos);
+
+                pos.x = (pos.x*2/ joystickImg.rectTransform.sizeDelta.x);
+                pos.y = (pos.y*2/ joystickImg.rectTransform.sizeDelta.y);
+
+                move = new Vector2(pos.x, pos.y);
+                move = (move.magnitude > 1.0f) ? move.normalized : move;
+
+                Debug.Log(move);
+
+
+                Vector2 joystickMove = new Vector2(move.x, move.y)*(joystickImg.rectTransform.sizeDelta.x/2);
+                joystickKnob.rectTransform.anchoredPosition = joystickMove;
+            }
         }
+
+
+            
         
     }
 
-
+    //Event: when screen is clicked down
     public virtual void OnPointerDown(PointerEventData ped) {
 
         Vector2 pos;
 
         if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos)) {
+
+            //enable joystick on click
+            //change position of joystick to click location
+            joystickKnob.enabled = true;
             joystickImg.enabled = true;
             joystickOrigin = new Vector2(pos.x, pos.y);
             joystickImg.rectTransform.anchoredPosition = joystickOrigin;
 
+            ////divide the coords to the size of joystick background and multiply by 2 to get coords max as 1
+            //pos.x = (pos.x * 2 / bgImg.rectTransform.sizeDelta.x);
+            //pos.y = (pos.y * 2 / bgImg.rectTransform.sizeDelta.y);
 
-            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
-            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
-
-
-
-
-            originPoint = new Vector3(pos.x * 2, 0, pos.y * 2);
-            originPoint = (originPoint.magnitude > 1.0f) ? originPoint.normalized : originPoint;
-
-            Debug.Log(originPoint);
-
-            
+            //originPoint = new Vector3(pos.x, 0, pos.y);
+            //originPoint = (originPoint.magnitude > 1.0f) ? originPoint.normalized : originPoint;           
 
         }
-
-
-
-
-
-
     }
 
+    //Event: when click is released
     public virtual void OnPointerUp(PointerEventData ped) {
 
         move = Vector3.zero;
         joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+        joystickKnob.rectTransform.anchoredPosition = Vector3.zero;
+        joystickKnob.enabled = false;
         joystickImg.enabled = false;
     }
     
@@ -129,9 +150,9 @@ public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, I
 
         public float Vertical() {
 
-        if (move.z != 0) {
+        if (move.y != 0) {
 
-            return move.z;
+            return move.y;
         }
         else {
 
