bf6d0d337be15c43961b3459f429f58f21a005c7
erjillsison <erjillsison@gmail.com>
2016-10-13 12:03:26 +0800

    new file:   .gitignore 	new file:   Assets/Code.meta 	new file:   Assets/Code/NewBehaviourScript.cs 	new file:   Assets/Code/NewBehaviourScript.cs.meta 	new file:   Assets/Code/PlayerController.cs 	new file:   Assets/Code/PlayerController.cs.meta 	new file:   Assets/Code/VirtualJoystick.cs 	new file:   Assets/Code/VirtualJoystick.cs.meta 	new file:   Assets/Code/VirtualJoystick1.cs 	new file:   Assets/Code/VirtualJoystick1.cs.meta 	new file:   Assets/MainScene.unity 	new file:   Assets/MainScene.unity.meta 	new file:   Assets/Materials.meta 	new file:   Assets/Materials/Ground.mat 	new file:   Assets/Materials/Ground.mat.meta 	new file:   Assets/Materials/Player.mat 	new file:   Assets/Materials/Player.mat.meta 	new file:   ProjectSettings/AudioManager.asset 	new file:   ProjectSettings/ClusterInputManager.asset 	new file:   ProjectSettings/DynamicsManager.asset 	new file:   ProjectSettings/EditorBuildSettings.asset 	new file:   ProjectSettings/EditorSettings.asset 	new file:   ProjectSettings/GraphicsSettings.asset 	new file:   ProjectSettings/InputManager.asset 	new file:   ProjectSettings/NavMeshAreas.asset 	new file:   ProjectSettings/NetworkManager.asset 	new file:   ProjectSettings/Physics2DSettings.asset 	new file:   ProjectSettings/ProjectSettings.asset 	new file:   ProjectSettings/ProjectVersion.txt 	new file:   ProjectSettings/QualitySettings.asset 	new file:   ProjectSettings/TagManager.asset 	new file:   ProjectSettings/TimeManager.asset 	new file:   ProjectSettings/UnityAdsSettings.asset 	new file:   ProjectSettings/UnityConnectSettings.asset 	new file:   config.php

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private float rotationSpeed = 1000;
+    private float walkSpeed = 10;
+    private float runSpeed = 5;
+
+    public VirtualJoystick joystick;
+    public VirtualJoystick1 joystickLook;
+    
+
+    private Quaternion targetRotation;
+
+
+    private  CharacterController controller;
+
+
+	// Use this for initialization
+	void Start () {
+
+        controller = GetComponent<CharacterController>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        Vector3 input = new Vector3 (joystick.Horizontal(), 0, joystick.Vertical());
+        Vector3 lookInput = new Vector3(joystickLook.Horizontal(), 0, joystickLook.Vertical());
+
+
+        if (lookInput != Vector3.zero) {
+            targetRotation = Quaternion.LookRotation(lookInput);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+        }
+
+        Vector3 motion = input.normalized;
+        motion += Vector3.up * -8;
+
+
+        controller.Move(motion*walkSpeed * Time.deltaTime);
+
+        
+	}
+}
---
+++ Assets/Code/VirtualJoystick1.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick1 : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
+
+    private Image bgImg;
+    private Image joystickImg;
+    private Vector3 originPoint;
+    private Vector3 dragPoint;
+    private Vector3 move;
+    private RectTransform canvas;
+    private RectTransform joystickRect;
+
+    private Vector2 canvasSize;
+    private Vector2 joystickOrigin;
+
+    // Use this for initialization
+    void Start () {
+
+        bgImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+        canvas = transform.parent.gameObject.GetComponent<RectTransform>();
+        joystickRect = GetComponent<RectTransform>();
+
+
+        joystickImg.enabled = false;
+
+        Color color = bgImg.color;
+        color.a = 0;
+        bgImg.color = color;
+
+        canvasSize = new Vector2(canvas.rect.width, canvas.rect.height);
+
+        Debug.Log(canvasSize);
+
+        transform.position = new Vector2(canvasSize.x*3/4,canvasSize.y/2);
+        joystickRect.sizeDelta = new Vector2(canvasSize.x/2,canvasSize.y);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void OnDrag(PointerEventData ped) {
+        Vector2 pos;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos)) {
+
+
+
+
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+
+
+
+            dragPoint = new Vector3(pos.x * 2, 0, pos.y * 2);
+            move = dragPoint - originPoint;
+           
+            move = (move.magnitude > 1.0f) ? move.normalized : move;
+            Debug.Log(move);
+
+
+
+            Vector2 joystickMove = new Vector2(move.x, move.z) * 100;
+            joystickImg.rectTransform.anchoredPosition = joystickOrigin + joystickMove;
+                ;
+        }
+        
+    }
+
+
+    public virtual void OnPointerDown(PointerEventData ped) {
+
+        Vector2 pos;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos)) {
+            joystickImg.enabled = true;
+            joystickOrigin = new Vector2(pos.x, pos.y);
+            joystickImg.rectTransform.anchoredPosition = joystickOrigin;
+
+
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+
+
+
+            originPoint = new Vector3(pos.x * 2, 0, pos.y * 2);
+            originPoint = (originPoint.magnitude > 1.0f) ? originPoint.normalized : originPoint;
+
+            Debug.Log(originPoint);
+
+            
+
+        }
+
+
+
+
+
+
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped) {
+
+        move = Vector3.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+        joystickImg.enabled = false;
+    }
+    
+    public float Horizontal() {
+
+        if (move.x != 0) {
+
+            return move.x;
+        }
+        else {
+
+            return Input.GetAxisRaw("Horizontal");
+        }
+    }
+
+        public float Vertical() {
+
+        if (move.z != 0) {
+
+            return move.z;
+        }
+        else {
+
+            return Input.GetAxisRaw("Vertical");
+        }
+    }
+    
+}
